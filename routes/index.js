553865fbb8519c21a0dacce8070854d83f07dc94
kakmond <wongsathorn.pa@ku.th>
2019-09-09 14:15:10 +0200

    finish song recommendation page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var request = require('request'); // "Request" library
+var request = require('request');
 var querystring = require('querystring');
 
 var client_id = 'b950e64dfc1948b6a6ce2018a9d154e8'; // Your client id
@@ -14,6 +14,7 @@ db.run("CREATE TABLE IF NOT EXISTS user (user_id INTEGER PRIMARY KEY, country TE
 db.run("CREATE TABLE IF NOT EXISTS track (user_id INTEGER PRIMARY KEY, track_1 TEXT, track_2 TEXT, track_3 TEXT, track_4 TEXT, track_5 TEXT, track_6 TEXT, track_7 TEXT, track_8 TEXT, track_9 TEXT, track_10 TEXT, FOREIGN KEY (user_id) REFERENCES user (user_id))");
 db.run("CREATE TABLE IF NOT EXISTS popularity25 (user_id INTEGER, track_id TEXT, recommendation_id TEXT, popularity INTEGER, FOREIGN KEY (user_id) REFERENCES user (user_id))");
 db.run("CREATE TABLE IF NOT EXISTS popularity75 (user_id INTEGER, track_id TEXT, recommendation_id TEXT, popularity INTEGER, FOREIGN KEY (user_id) REFERENCES user (user_id))");
+db.run("CREATE TABLE IF NOT EXISTS recommendation (user_id INTEGER, track_id TEXT, list INTEGER, answer_a INTEGER, waiting_a INTEGER, bot0_a INTEGER, bot1_a INTEGER, bot2_a INTEGER, bot3_a INTEGER, answer_b INTEGER, waiting_b INTEGER, bot0_b INTEGER, bot1_b INTEGER, bot2_b INTEGER, bot3_b INTEGER, FOREIGN KEY (user_id) REFERENCES user (user_id))");
 
 /**
  * Generates a random string containing numbers and letters
@@ -21,10 +22,10 @@ db.run("CREATE TABLE IF NOT EXISTS popularity75 (user_id INTEGER, track_id TEXT,
  * @return {string} The generated string
  */
 var generateRandomString = function (length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let text = '';
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-  for (var i = 0; i < length; i++) {
+  for (let i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
@@ -90,6 +91,25 @@ router.get('/song/:id', checkToken, function (req, res) {
   let user_id = req.cookies.user_id;
   // get track's detail
   request.get(trackOptions(req.params.id, access_token), function (error, response, trackBody) {
+    res.render('song', {
+      track: trackBody
+    });
+  });
+})
+
+router.get('/recommendation/:id', checkToken, function (req, res) {
+  // let sql25 = 'SELECT * FROM popularity25 WHERE track_id = ? AND user_id = ? '
+  // let sql75 = 'SELECT * FROM popularity75 WHERE track_id = ? AND user_id = ? '
+  // db.all(sql25, req.params.id, user_id, function (error, rows) {
+  //   randomTracks25 = rows
+  // })
+  // db.all(sql75, req.params.id, user_id, function (error, rows) {
+  //   randomTracks75 = rows
+  // })
+  let access_token = req.cookies.access_token;
+  let user_id = req.cookies.user_id;
+  // get track's detail
+  request.get(trackOptions(req.params.id, access_token), function (error, response, trackBody) {
     let artistIds = trackBody.artists.map((artist) => artist.id).join(',') // get artist id as array
     let trackId = trackBody.id
     // delete old popularity records if exist
@@ -97,22 +117,25 @@ router.get('/song/:id', checkToken, function (req, res) {
     db.run("DELETE FROM popularity75 WHERE track_id = ? ", trackId);
     // get recommendations based on popularity of 25 and 75
     request.get(recommendationOptions(access_token, artistIds, trackId, 25), function (error, response, popularity25body) {
+      randomTracks25 = []
       // store data in popularity25 table
       for (let index = 0; index < popularity25body.tracks.length; index++) {
         db.run("INSERT or REPLACE INTO popularity25 (user_id, track_id, recommendation_id, popularity) VALUES (?,?,?,?)", user_id, trackId, popularity25body.tracks[index].id, popularity25body.tracks[index].popularity);
+        randomTracks25.push(popularity25body.tracks[index].id)
       }
       request.get(recommendationOptions(access_token, artistIds, trackId, 75), function (error, response, popularity75body) {
+        randomTracks75 = []
         // store data in popularity75 table
         for (let index = 0; index < popularity75body.tracks.length; index++) {
           db.run("INSERT or REPLACE INTO popularity75 (user_id, track_id, recommendation_id, popularity) VALUES (?,?,?,?)", user_id, trackId, popularity75body.tracks[index].id, popularity75body.tracks[index].popularity);
+          randomTracks75.push(popularity25body.tracks[index].id)
         }
-        res.render('song', {
-          track: trackBody
-        });
+        res.render('recommendation', { randomTracks25: JSON.stringify(randomTracks25), randomTracks75: JSON.stringify(randomTracks75) });
       });
     });
   });
-})
+
+});
 
 router.get('/', checkToken, function (req, res) {
   let access_token = req.cookies.access_token;
@@ -197,6 +220,30 @@ router.get('/login', function (req, res) {
       state: state
     }));
 })
+
+router.post('/recommendation/', checkToken, function (req, res) {
+  let recommendationArray = req.body
+  for (let index = 0; index < recommendationArray.length; index++) {
+    let recommendationObject = recommendationArray[index]
+    let user_id = recommendationObject.user_id
+    let track_id = recommendationObject.track_id
+    let list = recommendationObject.list
+    let answer_a = recommendationObject.answer_a
+    let waiting_a = recommendationObject.waiting_a
+    let bot0_a = recommendationObject.bot0_a
+    let bot1_a = recommendationObject.bot1_a
+    let bot2_a = recommendationObject.bot2_a
+    let bot3_a = recommendationObject.bot3_a
+    let answer_b = recommendationObject.answer_b
+    let waiting_b = recommendationObject.waiting_b
+    let bot0_b = recommendationObject.bot0_b
+    let bot1_b = recommendationObject.bot1_b
+    let bot2_b = recommendationObject.bot2_b
+    let bot3_b = recommendationObject.bot3_b
+    db.run("INSERT INTO recommendation (user_id, track_id, list, answer_a, waiting_a, bot0_a, bot1_a, bot2_a, bot3_a, answer_b, waiting_b, bot0_b, bot1_b, bot2_b, bot3_b) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", user_id, track_id, list, answer_a, waiting_a, bot0_a, bot1_a, bot2_a, bot3_a, answer_b, waiting_b, bot0_b, bot1_b, bot2_b, bot3_b)
+  }
+}
+);
 
 function checkToken(req, res, next) {
   let token = req.cookies.access_token;
