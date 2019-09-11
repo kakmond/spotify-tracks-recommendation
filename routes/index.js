72cf1964a87e71db2d14f5d011d12e2892b60510
kakmond <wongsathorn.pa@ku.th>
2019-09-11 18:19:20 +0200

    change sqlite3 to mysql

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,26 @@ var querystring = require('querystring');
 var client_id = 'effb4e77d8764a29a23dc735f6f11dfa'; // Your client id
 var client_secret = 'ef52e8965b47466e812d2d3ce761e582'; // Your secret
 // var redirect_uri = 'https://spotify-group.herokuapp.com/callback'; // Your redirect uri
-var redirect_uri = 'https://spotify-group2.herokuapp.com/callback'; // Your redirect uri
+// var redirect_uri = 'https://spotify-group2.herokuapp.com/callback'; // Your redirect uri
+var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
 
-var sqlite3 = require('sqlite3');
-var db = new sqlite3.Database("spotifyDb");
+
+var mysql = require('mysql');
+var db = mysql.createConnection({
+  host: 'sql7.freemysqlhosting.net',
+  user: 'sql7304860',
+  password: 'JJ8VcE1ptp',
+  database: 'sql7304860'
+});
+
+db.connect();
+
 // create sqlite tables
-db.run("CREATE TABLE IF NOT EXISTS user (user_id INTEGER PRIMARY KEY, country TEXT, access_token TEXT, refresh_token TEXT)");
-db.run("CREATE TABLE IF NOT EXISTS track (user_id INTEGER PRIMARY KEY, track_1 TEXT, track_2 TEXT, track_3 TEXT, track_4 TEXT, track_5 TEXT, track_6 TEXT, track_7 TEXT, track_8 TEXT, track_9 TEXT, track_10 TEXT, FOREIGN KEY (user_id) REFERENCES user (user_id))");
-db.run("CREATE TABLE IF NOT EXISTS popularity25 (user_id INTEGER, track_id TEXT, recommendation_id TEXT, popularity INTEGER, FOREIGN KEY (user_id) REFERENCES user (user_id))");
-db.run("CREATE TABLE IF NOT EXISTS popularity75 (user_id INTEGER, track_id TEXT, recommendation_id TEXT, popularity INTEGER, FOREIGN KEY (user_id) REFERENCES user (user_id))");
-db.run("CREATE TABLE IF NOT EXISTS recommendation (user_id INTEGER, track_id TEXT, list INTEGER, answer_a INTEGER, waiting_a INTEGER, bot0_a INTEGER, bot1_a INTEGER, bot2_a INTEGER, bot3_a INTEGER, answer_b INTEGER, waiting_b INTEGER, bot0_b INTEGER, bot1_b INTEGER, bot2_b INTEGER, bot3_b INTEGER, FOREIGN KEY (user_id) REFERENCES user (user_id))");
+db.query("CREATE TABLE IF NOT EXISTS user (user_id INTEGER PRIMARY KEY, country TEXT, access_token TEXT, refresh_token TEXT)");
+db.query("CREATE TABLE IF NOT EXISTS track (user_id INTEGER PRIMARY KEY, track_1 TEXT, track_2 TEXT, track_3 TEXT, track_4 TEXT, track_5 TEXT, track_6 TEXT, track_7 TEXT, track_8 TEXT, track_9 TEXT, track_10 TEXT, FOREIGN KEY (user_id) REFERENCES user (user_id))");
+db.query("CREATE TABLE IF NOT EXISTS popularity25 (user_id INTEGER, track_id TEXT, recommendation_id TEXT, popularity INTEGER, FOREIGN KEY (user_id) REFERENCES user (user_id))");
+db.query("CREATE TABLE IF NOT EXISTS popularity75 (user_id INTEGER, track_id TEXT, recommendation_id TEXT, popularity INTEGER, FOREIGN KEY (user_id) REFERENCES user (user_id))");
+db.query("CREATE TABLE IF NOT EXISTS recommendation (user_id INTEGER, track_id TEXT, list INTEGER, answer_a INTEGER, waiting_a INTEGER, bot0_a INTEGER, bot1_a INTEGER, bot2_a INTEGER, bot3_a INTEGER, answer_b INTEGER, waiting_b INTEGER, bot0_b INTEGER, bot1_b INTEGER, bot2_b INTEGER, bot3_b INTEGER, FOREIGN KEY (user_id) REFERENCES user (user_id))");
 
 /**
  * Generates a random string containing numbers and letters
@@ -52,7 +62,7 @@ function authOptions(code) {
 
 function trackOptions(trackId, access_token) {
   return {
-    url: 'https://api.spotify.com/v1/tracks/' + trackId,
+    url: 'https://api.spotify.com/v1/tracks/' + trackId + '?market=SE',
     headers: { 'Authorization': 'Bearer ' + access_token },
     json: true
   }
@@ -62,18 +72,18 @@ function getTopTracksOptions(access_token) {
   return {
     url: 'https://api.spotify.com/v1/me/top/tracks?limit=10',
     headers: { 'Authorization': 'Bearer ' + access_token },
-    json: true
+    json: true,
   }
 }
 
 
-function recommendationOptions(access_token, artistId, trackId, popularity) {
+function recommendationOptions(access_token, trackId, popularity) {
   return {
     url: 'https://api.spotify.com/v1/recommendations?' +
       querystring.stringify({
-        seed_artists: artistId,
         seed_tracks: trackId,
         target_popularity: popularity,
+        market: 'SE'
       }),
     headers: { 'Authorization': 'Bearer ' + access_token },
     json: true
@@ -100,36 +110,27 @@ router.get('/song/:id', checkToken, function (req, res) {
 })
 
 router.get('/recommendation/:id', checkToken, function (req, res) {
-  // let sql25 = 'SELECT * FROM popularity25 WHERE track_id = ? AND user_id = ? '
-  // let sql75 = 'SELECT * FROM popularity75 WHERE track_id = ? AND user_id = ? '
-  // db.all(sql25, req.params.id, user_id, function (error, rows) {
-  //   randomTracks25 = rows
-  // })
-  // db.all(sql75, req.params.id, user_id, function (error, rows) {
-  //   randomTracks75 = rows
-  // })
   let access_token = req.cookies.access_token;
   let user_id = req.cookies.user_id;
   // get track's detail
   request.get(trackOptions(req.params.id, access_token), function (error, response, trackBody) {
-    let artistIds = trackBody.artists.map((artist) => artist.id).join(',') // get artist id as array
     let trackId = trackBody.id
     // delete old popularity records if exist
-    db.run("DELETE FROM popularity25 WHERE track_id = ? ", trackId);
-    db.run("DELETE FROM popularity75 WHERE track_id = ? ", trackId);
+    db.query("DELETE FROM popularity25 WHERE track_id = ? ", [trackId]);
+    db.query("DELETE FROM popularity75 WHERE track_id = ? ", [trackId]);
     // get recommendations based on popularity of 25 and 75
-    request.get(recommendationOptions(access_token, artistIds, trackId, 25), function (error, response, popularity25body) {
+    request.get(recommendationOptions(access_token, trackId, 25), function (error, response, popularity25body) {
       randomTracks25 = []
+      randomTracks75 = []
       // store data in popularity25 table
       for (let index = 0; index < popularity25body.tracks.length; index++) {
-        db.run("INSERT or REPLACE INTO popularity25 (user_id, track_id, recommendation_id, popularity) VALUES (?,?,?,?)", user_id, trackId, popularity25body.tracks[index].id, popularity25body.tracks[index].popularity);
+        db.query("REPLACE INTO popularity25 (user_id, track_id, recommendation_id, popularity) VALUES (?,?,?,?)", [user_id, trackId, popularity25body.tracks[index].id, popularity25body.tracks[index].popularity])
         randomTracks25.push(popularity25body.tracks[index].id)
       }
-      request.get(recommendationOptions(access_token, artistIds, trackId, 75), function (error, response, popularity75body) {
-        randomTracks75 = []
+      request.get(recommendationOptions(access_token, trackId, 75), function (error, response, popularity75body) {
         // store data in popularity75 table
         for (let index = 0; index < popularity75body.tracks.length; index++) {
-          db.run("INSERT or REPLACE INTO popularity75 (user_id, track_id, recommendation_id, popularity) VALUES (?,?,?,?)", user_id, trackId, popularity75body.tracks[index].id, popularity75body.tracks[index].popularity);
+          db.query("REPLACE INTO popularity75 (user_id, track_id, recommendation_id, popularity) VALUES (?,?,?,?)", [user_id, trackId, popularity75body.tracks[index].id, popularity75body.tracks[index].popularity])
           randomTracks75.push(popularity25body.tracks[index].id)
         }
         res.render('recommendation', { randomTracks25: JSON.stringify(randomTracks25), randomTracks75: JSON.stringify(randomTracks75) });
@@ -146,12 +147,15 @@ router.get('/', checkToken, function (req, res) {
   // retrive profile information
   request.get(profileOptions(access_token), function (error, response, body) {
     // store data in user table
-    db.run("INSERT or REPLACE INTO user (user_id, country, access_token, refresh_token) VALUES (?,?,?,?)", body.id, body.country, access_token, refresh_token);
-    res.render('index', {
-      display_name: body.display_name, country: body.country,
-      email: body.email, id: body.id, href: body.href, external_urls: body.external_urls,
-      images: body.images
-    });
+    db.query("REPLACE INTO user (user_id, country, access_token, refresh_token) VALUES (?,?,?,?)", [body.id, body.country, access_token, refresh_token], function (err, result) {
+      console.log(err)
+      res.render('index', {
+        display_name: body.display_name, country: body.country,
+        email: body.email, id: body.id, href: body.href, external_urls: body.external_urls,
+        images: body.images
+      });
+    })
+
   });
 });
 
@@ -189,7 +193,7 @@ router.get('/tracks', checkToken, function (req, res) {
   request.get(getTopTracksOptions(access_token), function (error, response, tracks) {
     let trackSQL = tracks.items.map((track) => track.id); // construct an array of ids
     trackSQL.unshift(user_id) // add user's id at the beginning of an array
-    db.run("INSERT or REPLACE INTO track (user_id, track_1, track_2, track_3, track_4, track_5, track_6, track_7, track_8, track_9, track_10) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", trackSQL);
+    db.query("REPLACE INTO track (user_id, track_1, track_2, track_3, track_4, track_5, track_6, track_7, track_8, track_9, track_10) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", trackSQL);
     res.render('tracks', {
       tracks: tracks.items
     });
@@ -242,7 +246,7 @@ router.post('/recommendation/', checkToken, function (req, res) {
     let bot1_b = recommendationObject.bot1_b
     let bot2_b = recommendationObject.bot2_b
     let bot3_b = recommendationObject.bot3_b
-    db.run("INSERT INTO recommendation (user_id, track_id, list, answer_a, waiting_a, bot0_a, bot1_a, bot2_a, bot3_a, answer_b, waiting_b, bot0_b, bot1_b, bot2_b, bot3_b) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", user_id, track_id, list, answer_a, waiting_a, bot0_a, bot1_a, bot2_a, bot3_a, answer_b, waiting_b, bot0_b, bot1_b, bot2_b, bot3_b)
+    db.query("INSERT INTO recommendation (user_id, track_id, list, answer_a, waiting_a, bot0_a, bot1_a, bot2_a, bot3_a, answer_b, waiting_b, bot0_b, bot1_b, bot2_b, bot3_b) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", [user_id, track_id, list, answer_a, waiting_a, bot0_a, bot1_a, bot2_a, bot3_a, answer_b, waiting_b, bot0_b, bot1_b, bot2_b, bot3_b])
   }
 }
 );
@@ -252,7 +256,7 @@ function checkToken(req, res, next) {
   if (token) {
     // try to retrive profile information
     request.get(profileOptions(token), function (error, response, body) {
-      if (error) {
+      if (body.error) {
         res.render('login')
       }
       else {
